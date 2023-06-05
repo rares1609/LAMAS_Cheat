fb32c4ca14e2c824212a469c0205352d6fbad1d4
AdrianSerbanescu <79575675+AdrianSerbanescu@users.noreply.github.com>
2023-06-05 15:03:38 +0200

    Update test_game.py
    Finished implementing a version of the game with only human players

--- test_game.py
+++ test_game.py
@@ -9,44 +9,44 @@ class Card:
         return f"{self.rank} of {self.suit}"
 
 class Player:
-    def __init__(self, name, play_strategy, call_bluff_strategy):
+    def __init__(self, name, play_strategy, call_bluff_strategy, is_human= False):
         self.name = name
         self.hand = []
         self.play_strategy = play_strategy
         self.call_bluff_strategy = call_bluff_strategy
+        self.is_human = is_human
 
     def play_card(self):
-        return self.play_strategy(self)
+        if self.is_human:
+            print(f"Your hand: {self.hand}")
+            action = input("Type the rank of the card you want to play: ").strip()
+            for card in self.hand:
+                if card.rank == action:
+                    self.hand.remove(card)
+                    return Card(action, card.suit)  # Return a new card with the claimed rank
+            print("Invalid input. You don't have that rank. You lose your turn.")
+            return None
+        else:
+            return self.play_strategy(self)
 
     def call_bluff(self, game):
-        return self.call_bluff_strategy(self, game)
+        if self.is_human:
+            return input("Do you want to call a bluff? (yes/no): ").strip().lower() == "yes"
+        else:
+            return self.call_bluff_strategy(self, game)
+        
+    def truth_or_lie(self):
+        card_to_play_rank = input("What rank do you want to claim the card has? ").strip()
+        print("Human claimed:", card_to_play_rank)
+        return card_to_play_rank
+
 
     def take_pile(self, pile):
-        self.hand.extend(pile)
+        self.hand.extend(pile.copy())
 
     def __repr__(self):
         return self.name
 
-def play_strategy_0th_order(player):
-    if game.current_rank is None:
-        card = random.choice(player.hand)
-        player.hand.remove(card)
-        return card
-
-    for card in player.hand:
-        if card.rank == game.current_rank:
-            player.hand.remove(card)
-            return card
-
-    # If the player doesn't have a card of the current rank, lie
-    card = random.choice(player.hand)
-    player.hand.remove(card)
-    return card
-
-def call_bluff_strategy_0th_order(player, game):
-    if game.current_rank is not None and not any(card.rank == game.current_rank for card in player.hand):
-        return True  # Call bluff if player does not have a card of the current rank
-    return False  # Otherwise, do not call bluff
 
 class Game:
     def __init__(self, players):
@@ -56,29 +56,40 @@ class Game:
         self.current_player_index = random.randint(0, len(self.players)-1)
 
     def deal(self):
-        deck = [Card(rank, suit) for rank in ['A', 'K', 'Q', 'J'] for suit in ['Red Heart', 'Black Heart', 'Diamond']]
+        deck = [Card(rank, suit) for rank in ['A', 'K', 'Q'] for suit in ['Red Heart', 'Black Heart', 'Diamond']]
         random.shuffle(deck)
         while deck:
             for player in self.players:
                 if deck:
                     player.hand.append(deck.pop())
+        
+        for player in self.players:
+            print(f"\n{player}'s hand: {player.hand}")
 
     def play_turn(self):
+        ok = False
         # Get the current player
-        current_player = self.players[self.current_player_index]
+        
+        current_player = self.players[self.current_player_index] # it clashes with this  one 
+
+        # Player's current turn print
+        print(f"\nIt's {current_player}'s turn.")
         
         # The current player plays a card
         card = current_player.play_card()
+
         
         if card:
             # If a card is played, add it to the pile and update the current rank
             self.pile.append(card)
-            self.current_rank = card.rank
+            if len(self.pile) == 1:
+                self.current_rank = current_player.truth_or_lie()
+            print(f"Current rank is: {self.current_rank}")
             print(f"{current_player} played {card}")
             
             # Get the next player
             next_player = self.players[(self.current_player_index+1)%len(self.players)]
-            
+            print(f"\nIt's {next_player}'s turn.")
             # The next player calls a bluff
             if next_player.call_bluff(self):
                 print(f"{next_player} calls bluff on {current_player}")
@@ -89,6 +100,8 @@ class Game:
                     # The next player was wrong to call a bluff, so they take the pile
                     next_player.take_pile(self.pile)
                     print(f"{next_player} was wrong. {next_player} takes the pile.")
+                    ok = True
+                    
                 else:  
                     # The bluff was true, the current player was caught lying
                     # They take the pile
@@ -102,8 +115,9 @@ class Game:
                 self.current_rank = None 
 
         # Move on to the next player
-        self.current_player_index = (self.current_player_index + 1) % len(self.players)
-
+        if ok == False:
+            self.current_player_index = (self.current_player_index + 1) % len(self.players)
+        
 
 
     def game_over(self):
@@ -119,11 +133,103 @@ class Game:
             self.play_turn()
             winner = self.game_over()
         print(f"{winner} is the winner!")
-        
 
-player1 = Player('Player1', play_strategy_0th_order, call_bluff_strategy_0th_order)
-player2 = Player('Player2', play_strategy_0th_order, call_bluff_strategy_0th_order)
-player3 = Player('Player3', play_strategy_0th_order, call_bluff_strategy_0th_order)
 
-game = Game([player1, player2, player3])
-game.play_game()
+# STRATEGY FUNCTIONS
+
+# implement this strategy
+def play_strategy_trusting(player):
+    if game.current_rank is None:
+        card = random.choice(player.hand)
+        player.hand.remove(card)
+        return card
+    # Generate a random number between 0 and 1
+    random_number = random.random()
+    if random_number <= 0.1:
+        # If the player doesn't have a card of the current rank, lie
+        card = random.choice(player.hand)
+        player.hand.remove(card)
+        return card
+
+    for card in player.hand:
+        if card.rank == game.current_rank:
+            player.hand.remove(card)
+            return card
+
+    # If the player doesn't have a card of the current rank, lie
+    card = random.choice(player.hand)
+    player.hand.remove(card)
+    return card
+
+# implement this strategy
+def play_strategy_distrusting(player):
+    if game.current_rank is None:
+        card = random.choice(player.hand)
+        player.hand.remove(card)
+        return card
+
+    for card in player.hand:
+        if card.rank == game.current_rank:
+            player.hand.remove(card)
+            return card
+
+    # If the player doesn't have a card of the current rank, lie
+    card = random.choice(player.hand)
+    player.hand.remove(card)
+    return card
+
+# implement this strategy
+def call_bluff_strategy_trusting(player, game):
+     # Call bluff or lie if player does not have a card of the current rank
+    if game.current_rank is not None and not any(card.rank == game.current_rank for card in player.hand):
+        # Generate a random number between 0 and 1
+        random_number = random.random()
+
+        if random_number <= 0.3:
+            # Call bluff
+            print("Agent says Cheat")
+            return True
+        else:
+            # Lie
+            return False
+    return False  # Otherwise, do not call bluff
+
+# implement this strategy
+def call_bluff_strategy_distrusting(player, game):
+    # Call bluff or lie if player does not have a card of the current rank
+    if game.current_rank is not None and not any(card.rank == game.current_rank for card in player.hand):
+        # Generate a random number between 0 and 1
+        random_number = random.random()
+
+        if random_number >= 0.3:
+            # Call bluff
+            print("Agent says Cheat")
+            return True
+            
+        else:
+            # Lie
+            return False
+    else: 
+        if game.current_rank is not None and any(card.rank == game.current_rank for card in player.hand):
+            # Generate a random number between 0 and 1
+            random_number = random.random()
+            if random_number <= 0.3:
+                # Call bluff
+                print("Agent says Cheat")
+                return True
+            else:
+                # Lie or Truth
+                return False
+            
+
+if __name__ == "__main__":
+    player1 = Player('Player 1', play_strategy_trusting, call_bluff_strategy_trusting, is_human= True)
+    player2 = Player('Player 2', play_strategy_trusting, call_bluff_strategy_trusting, is_human= True)
+    player3 = Player('Player 3', play_strategy_distrusting, call_bluff_strategy_distrusting, is_human= True)
+
+    game = Game([player1, player2, player3])
+
+    game.play_game()
+
+
+
